015e200399780caf167fe05ec8cf313aec065bf5
jrealesdelaossa <jrealesdelaossa@gmail.com>
2023-02-12 15:26:25 -0500

    primer algoritmo terminado

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,45 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Hola mundo"/> -->
-    <primer-algoritmo />
+    <div class="cabecera">
+      <label class="text-selector" for="selector">Seleccione el algoritmo que desea revisar</label>
+      <select class="selector-opciones" name="selector" id="selector" v-model="algoritmoSeleccionado" @change="seleccionar">
+        <option v-for="p in prices" v-bind:key="p">{{ p }}</option>
+      </select>
+    </div>
+
+    <div v-if="primero" class="show-algoritmo">
+      <primer-algoritmo />
+    </div>
+    <br>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import PrimerAlgoritmo from "@/components/primerAlgoritmo.vue";
-
 export default {
   name: 'App',
   components: {
     PrimerAlgoritmo,
     // HelloWorld
+  },
+  data() {
+    return {
+      prices: ["Suma de dos números", "segundo"],
+      algoritmoSeleccionado: "",
+      primero: undefined,
+      segundo: undefined,
+    }
+  },
+  methods: {
+    seleccionar() {
+      if (this.algoritmoSeleccionado === "Suma de dos números") {
+        this.primero = true;
+      } else {
+        this.primero = false;
+      }
+    }
   }
 }
 </script>
@@ -31,5 +57,32 @@ body {
   color: #2c3e50;
   margin-top: 60px;
 
+}
+
+.cabecera {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+}
+
+.text-selector {
+  font-size: 24px;
+}
+
+.selector-opciones {
+  margin-left: 24px;
+  width: 400px;
+  height: 40px;
+  font-size: 16px;
+  border: 4px solid #00af00;
+  border-radius: 12px;
+}
+
+.show-algoritmo {
+  display: grid;
+  place-content: center;
+  margin-top: 40px;
 }
 </style>
---
+++ src/components/selectorAlgoritmo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <select name="selector" id="selector" v-model="algoritmoSeleccionado">
+      <option v-for="p in prices" v-bind:key="p">{{ p }}</option>
+    </select>
+    {{ algoritmoSeleccionado }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "selectorAlgoritmo",
+  data() {
+    return {
+      prices: ["Suma de dos números", "segundo"],
+      algoritmoSeleccionado: "",
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
