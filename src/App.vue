8bcec5db18248d8771ccee43c9eabe1314f87b6d
jrealesdelaossa <jrealesdelaossa@gmail.com>
2023-02-12 15:57:13 -0500

    segundo algoritmo terminado

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@
     <div class="cabecera">
       <label class="text-selector" for="selector">Seleccione el algoritmo que desea revisar</label>
       <select class="selector-opciones" name="selector" id="selector" v-model="algoritmoSeleccionado" @change="seleccionar">
-        <option v-for="p in prices" v-bind:key="p">{{ p }}</option>
+        <option v-for="p in algoritmos" v-bind:key="p">{{ p }}</option>
       </select>
     </div>
 
     <div v-if="primero" class="show-algoritmo">
       <primer-algoritmo />
+    </div>
+    <div v-if="segundo" class="show-algoritmo">
+      <segundo-algoritmo />
     </div>
     <br>
   </div>
@@ -19,15 +22,17 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import PrimerAlgoritmo from "@/components/primerAlgoritmo.vue";
+import SegundoAlgoritmo from "@/components/segundoAlgoritmo.vue";
 export default {
   name: 'App',
   components: {
+    SegundoAlgoritmo,
     PrimerAlgoritmo,
     // HelloWorld
   },
   data() {
     return {
-      prices: ["Suma de dos números", "segundo"],
+      algoritmos: ["Suma de dos números", "Operación matemática"],
       algoritmoSeleccionado: "",
       primero: undefined,
       segundo: undefined,
@@ -37,8 +42,15 @@ export default {
     seleccionar() {
       if (this.algoritmoSeleccionado === "Suma de dos números") {
         this.primero = true;
+
+        this.segundo = false;
+      } else if (this.algoritmoSeleccionado === "Operación matemática") {
+        this.segundo = true;
+
+        this.primero = false;
       } else {
         this.primero = false;
+        this.segundo = false;
       }
     }
   }
---
+++ src/components/segundoAlgoritmo.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="main">
+    <h2>Operación matemática</h2>
+    <p>Ingresar dos números enteros, el resultado sera la multiplicación de esos dos números menos el primero</p>
+
+    <div class="entrada">
+      <label for="num1">Primer número: </label>
+      <input type="number" name="num1" id="num1" v-model="numero1" >
+    </div>
+    <br>
+    <div class="entrada">
+      <label for="num2">Segundo número: </label>
+      <input type="number" name="num2" id="num2" v-model="numero2" >
+    </div>
+
+    <span> = {{ resultado }}</span>
+
+    <div class="boton-calcular">
+      <button @click="calcular">Calcular</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "segundoAlgoritmo",
+  data() {
+    return {
+      resultado: 0,
+      numero1: 0,
+      numero2: 0,
+    }
+  },
+  methods: {
+    calcular(){
+      this.resultado = (parseInt(this.numero1) * parseInt(this.numero2)) - this.numero1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  background-color: #00af00;
+  border: 12px solid #FFF;
+  border-radius: 24px;
+  color: #FFFFFF;
+}
+
+.entrada {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 40px;
+}
+
+h2 {
+  text-transform: uppercase;
+}
+
+input {
+  border: none;
+  background: #cdefcd;
+  color: #000000;
+  /* font-size: 16px; */
+  text-align: center;
+  /* width: 80px; */
+  border-radius: 4px;
+}
+
+span {
+  margin: 24px;
+  font-size: 50px;
+}
+
+.boton-calcular {
+  margin: 0 24px 24px 24px;
+}
+
+.boton-calcular button {
+  width: 40%;
+  height: 40px;
+  border: none;
+  border-radius: 12px;
+  font-size: 20px;
+  text-transform: uppercase;
+}
+
+.boton-calcular button:hover {
+  scale: 110%;
+  cursor: pointer;
+}
+</style>
